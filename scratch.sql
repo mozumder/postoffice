f61e2b108a9fe9d4625f2a0ceff2e90ae3ee8fac
Bobby Mozumder <mozumder@futureclaw.com>
2020-04-23 19:01:54 -0400

    Testing SQL

--- scratch.sql
+++ scratch.sql
@@ -25,9 +25,8 @@ where
     dns_aaaa_record.fqdn != 'la.mozumder.net' AND
     'la.mozumder.net' LIKE '%' || dns_domain.name ;
 ;
-
 select
-    6 as type,
+    24 as type,
     dns_aaaa_record.ttl as ttl,
     ip_address,
     dns_domain.name as domainname,
@@ -46,4 +45,38 @@ on
     dns_domain.id = dns_aaaa_record.domain_id
 where
     dns_aaaa_record.fqdn = 'la.mozumder.net'
+;
+
+select
+    6 as type,
+    dns_aaaa_record.name as name,
+    ip_address,
+    dns_aaaa_record.ttl as ttl,
+    dns_domain.name as domainname,
+    dns_soa_record.id as soa_id,
+    dns_soa_record.nameserver as nsname,
+    dns_soa_record.rname as rname,
+    dns_soa_record.serial as serial,
+    dns_soa_record.refresh as refresh,
+    dns_soa_record.retry as retry,
+    dns_soa_record.expiry as expiry,
+    dns_soa_record.nxttl as nxttl,
+    dns_soa_record.domain_id,
+    dns_aaaa_record.domain_id
+from
+    dns_aaaa_record, dns_domain, dns_soa_record
+where
+    (dns_aaaa_record.fqdn != 'mozumder.net' AND
+    'mozumder.net' LIKE '%' || dns_domain.name AND
+    dns_aaaa_record.domain_id = dns_domain.id AND
+    dns_soa_record.domain_id = dns_domain.id ) OR
+    (dns_aaaa_record.fqdn != 'mozumder.net' AND
+    'mozumder.net' LIKE '%' || dns_domain.name AND
+    dns_aaaa_record.domain_id = dns_domain.id AND
+    dns_soa_record.domain_id = dns_domain.id ) OR
+
+    dns_soa_record.domain_id != dns_aaaa_record.domain_id AND
+;
+    AND
+;
 
